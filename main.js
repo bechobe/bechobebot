3e29e3f12040b1a1519749893a9b71c85e47a1f2
bechobe <bechobe@hotmail.com>
2018-11-20 05:32:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ client.on("ready", () => {
   if (message.content.startsWith("!s5m")) {
     let modrole = message.guild.roles.find("name", "Organizadores");
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 5 MINUTOS. Suban a la sala Sincronización!**.");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 5 MINUTOS. Suban a la sala Sincronización!**.");
     }
   }
 });
@@ -20,7 +20,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!s4m")) {
     let modrole = message.guild.roles.find("name", "Organizadores");
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 4 MINUTOS. Suban a la sala Sincronización!**.");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 4 MINUTOS. Suban a la sala Sincronización!**.");
     }
   }
 });
@@ -29,7 +29,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!s3m")) {
     let modrole = message.guild.roles.find("name", "Organizadores" );
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 3 MINUTOS. Suban a la sala Sincronización!.**");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 3 MINUTOS. Suban a la sala Sincronización!.**");
     }
   }
 });
@@ -38,7 +38,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!s2m")) {
     let modrole = message.guild.roles.find("name", "Organizadores" );
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 2 MINUTOS. Suban a la sala Sincronización!.**");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 2 MINUTOS. Suban a la sala Sincronización!.**");
     }
   }
 });
@@ -47,7 +47,16 @@ client.on("message", (message) => {
   if (message.content.startsWith("!s1m")) {
     let modrole = message.guild.roles.find("name", "Organizadores" );
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 1 MINUTO. Suban a la sala Sincronización!**.");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 1 MINUTO. Suban a la sala Sincronización!**.");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!s30s")) {
+    let modrole = message.guild.roles.find("name", "Organizadores" );
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 30 SEGUNDOS. Suban a la sala Sincronización!**.");
     }
   }
 });
@@ -56,7 +65,16 @@ client.on("message", (message) => {
   if (message.content.startsWith("!s20s")) {
     let modrole = message.guild.roles.find("name", "Organizadores" );
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 20 SEGUNDOS. Suban a la sala Sincronización!**.");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 20 SEGUNDOS. Suban a la sala Sincronización!**.");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!s15s")) {
+    let modrole = message.guild.roles.find("name", "Organizadores" );
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 15 SEGUNDOS. Suban a la sala Sincronización!**.");
     }
   }
 });
@@ -65,7 +83,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!s10s")) {
     let modrole = message.guild.roles.find("name", "Organizadores")
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 10 SEGUNDOS. Suban a la sala Sincronización!**");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 10 SEGUNDOS. Suban a la sala Sincronización!**");
     }
   }
 });
@@ -109,21 +127,8 @@ client.on("message", (message) => {
 client.on('message', message => {
   var sender = message.member;
   var prefix = '>'
-  if (message.content.startsWith("!mid")) {
-  if (message.channel.id === '513157741657849877') {
-    if(message.content.length != 8) {
-      message.delete()
-      message.member.send('Solamente se puede escribir el codigo de tu lobby, si quieres decir algo por favor usa el chat scrims, gracias!')
-    }
-  }
-  }
-});
-
-client.on('message', message => {
-  var sender = message.member;
-  var prefix = '>'
-  if (message.channel.id === '513157741657849877') {
-    if(message.content.length != 8) {
+  if (message.channel.id === '513827541430304816') {
+    if(message.content.length != 4) {
       message.delete()
       message.member.send('Solamente se puede escribir el codigo de tu lobby, si quieres decir algo por favor usa el chat scrims, gracias!')
     }
@@ -134,16 +139,96 @@ client.on('message', message => {
   if (message.content.startsWith("!s30s")) {
     let modrole = message.guild.roles.find("name", "Organizadores");
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 30 SEGUNDOS!. Suban a la sala Sincronización**.");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 30 SEGUNDOS!. Suban a la sala Sincronización**.");
         
  client.on("message", (message) => {
   if (message.content.startsWith("!s15s")) {
     let modrole = message.guild.roles.find("name", "Organizadores");
     if(message.member.roles.has(modrole.id)) {
-    message.channel.send("@here **La próxima partida comienza en 15 SEGUNDOS!. Suban a la sala Sincronización**.");
+    message.channel.send("@here **La próxima partida de __SOLO SCRIMS__ comienza en 15 SEGUNDOS!. Suban a la sala Sincronización**.");
     }
   }
 });
 
+//Que buenas empanadas que hace la mama de hydr4
+
+ client.on("message", (message) => {
+  if (message.content.startsWith("!d5m")) {
+    let modrole = message.guild.roles.find("name", "Organizadores");
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 5 MINUTOS. Suban a la sala Sincronización!**.");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!d4m")) {
+    let modrole = message.guild.roles.find("name", "Organizadores");
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 4 MINUTOS. Suban a la sala Sincronización!**.");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!d3m")) {
+    let modrole = message.guild.roles.find("name", "Organizadores" );
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 3 MINUTOS. Suban a la sala Sincronización!.**");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!d2m")) {
+    let modrole = message.guild.roles.find("name", "Organizadores" );
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 2 MINUTOS. Suban a la sala Sincronización!.**");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!d1m")) {
+    let modrole = message.guild.roles.find("name", "Organizadores" );
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 1 MINUTO. Suban a la sala Sincronización!**.");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!d20s")) {
+    let modrole = message.guild.roles.find("name", "Organizadores" );
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 20 SEGUNDOS. Suban a la sala Sincronización!**.");
+    }
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!d10s")) {
+    let modrole = message.guild.roles.find("name", "Organizadores")
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 10 SEGUNDOS. Suban a la sala Sincronización!**");
+    }
+  }
+});
+     
+ client.on("message", (message) => {
+  if (message.content.startsWith("!d30s")) {
+    let modrole = message.guild.roles.find("name", "Organizadores");
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 30 SEGUNDOS!. Suban a la sala Sincronización**.");
+        
+ client.on("message", (message) => {
+  if (message.content.startsWith("!d15s")) {
+    let modrole = message.guild.roles.find("name", "Organizadores");
+    if(message.member.roles.has(modrole.id)) {
+    message.channel.send("@here **La próxima partida de __DUO SCRIMS__ comienza en 15 SEGUNDOS!. Suban a la sala Sincronización**.");
+    }
+  }
+});        
+      
 
 client.login(config.token);   
